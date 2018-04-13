c176a8b977a2e1c4e41bed10576b46d73d291331
WANGXUANDING <wangxuanding1@gmail.com>
2018-04-13 23:42:47 +0200

    7-5

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy, :join, :quit]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
@@ -42,6 +42,30 @@ class GroupsController < ApplicationController
 
     flash[:alert]= "Group deleted"
     redirect_to groups_path
+  end
+
+  def join
+    @group = Group.find(params[:id])
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice]="+"
+    else
+      flash[:warning]="already"
+    end
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert]="-"
+    else
+      flash[:warning] = "not a member"
+    end
+
+    redirect_to group_path(@group)
   end
 
   private
